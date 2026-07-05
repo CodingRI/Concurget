93b6cd366b07ce6ed34d1a15e8f14ef1129a2f5d
CodingRI <125729609+CodingRI@users.noreply.github.com>
2026-07-06 01:30:04 +0530

    worker pool, context support, and result tracking

--- downloader/downloader.go
+++ downloader/downloader.go
@@ -1,6 +1,7 @@
 package downloader
 
 import (
+	"context"
 	"io"
 	"net/http"
 	"os"
@@ -12,24 +13,47 @@ import (
 	"fmt"
 )
 
-func Download(rawURL string) error {
+func Download(ctx context.Context, rawURL string) Result {
 
-		resp, err := http.Get(rawURL)
-		
-		if err != nil {
-			return err
-		}
+	req, err := http.NewRequestWithContext(
+		ctx,
+		http.MethodGet,
+		rawURL,
+		nil,
+	)
 
-		
-		defer resp.Body.Close()
-		
-		if resp.StatusCode != http.StatusOK {
-			return fmt.Errorf("unexpected status: %d", resp.StatusCode);
+	if err != nil {
+		return Result{
+			URL: rawURL,
+			Err: err,
 		}
+	}
+
+	client := &http.Client{}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return Result{
+			URL: rawURL,
+			Err: err,
+		}
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return Result{
+			URL: rawURL,
+			Err: fmt.Errorf("unexpected status: %d", resp.StatusCode),
+		}
+	}
 
 		u, err := url.Parse(rawURL)
 		if err != nil {
-			return err
+			return Result {
+				URL : rawURL,
+				Err: err,
+			}
 		}
 
 		fileName := path.Base(u.Path)
@@ -38,9 +62,19 @@ func Download(rawURL string) error {
 		if fileName == "." || fileName == "/" {
 			fileName = "download.bin"
 		}
-		file, err := os.Create(fileName)
+		
+
+		os.MkdirAll("Downloads", 0755)
+
+		filePath := path.Join("Downloads", fileName)
+
+		file, err := os.Create(filePath)
+
 		if err != nil {
-			return err
+			return Result {
+				URL : rawURL,
+				Err: err,
+			}
 		}
 
 		defer file.Close()
@@ -48,10 +82,20 @@ func Download(rawURL string) error {
 		bytesWritten, err := io.Copy(file, resp.Body)
 
 		if err != nil {
-			return err
+			return Result {
+				URL : rawURL,
+				Err: err,
+			}
 		}
 
 		fmt.Printf("%s downloaded (%d bytes)\n", fileName, bytesWritten)
-		return nil
+		
+		
+		return Result{
+			URL:      rawURL,
+			Filename: fileName,
+			Bytes:    bytesWritten,
+			Err:      nil,
+		}
 	
 }
