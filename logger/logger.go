c33b52e484f8520d61e9aa8a077a919859fd1b73
CodingRI <125729609+CodingRI@users.noreply.github.com>
2026-07-06 02:14:58 +0530

    adding custom loggers

---
+++ logger/logger.go
@@ -0,0 +1,18 @@
+package logger
+
+import (
+	"log"
+	"os"
+)
+
+var Info = log.New(
+	os.Stdout,
+	"[INFO] ",
+	log.LstdFlags,
+)
+
+var Error = log.New(
+	os.Stderr,
+	"[ERROR] ",
+	log.LstdFlags,
+)
