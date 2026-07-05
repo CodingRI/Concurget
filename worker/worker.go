0d67f105d99b5b0320cf8659f61e171447542f1c
CodingRI <125729609+CodingRI@users.noreply.github.com>
2026-07-06 01:51:14 +0530

    implement context-aware worker termination in worker.go

--- worker/worker.go
+++ worker/worker.go
@@ -8,11 +8,25 @@ import (
 
 
 func Start(ctx context.Context, id int, jobs <-chan string, results chan<- downloader.Result) {
-	for url := range jobs {
-		fmt.Printf("[Worker %d] downloading %s\n", id, url)
+	for {
 
-		result := downloader.Download(ctx, url)
-
-		results <- result
+		select {
+	
+		case <-ctx.Done():
+			fmt.Printf("[Worker %d] stopping...\n", id)
+			return
+	
+		case url, ok := <-jobs:
+	
+			if !ok {
+				return
+			}
+	
+			fmt.Printf("[Worker %d] downloading %s\n", id, url)
+	
+			result := downloader.Download(ctx, url)
+	
+			results <- result
+		}
 	}
 }
